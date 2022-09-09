8f458a6f05a65a6ced8f27c01d02a58ebf1b71a5
Owen Chen <ochen@bayviewglen.ca>
2022-09-09 11:08:54 -0400

    Added day2 package with ExampleOne and ExampleTwo.

---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleOne.java
@@ -0,0 +1,21 @@
+package day2;
+
+//This is a multi line comment
+
+public class ExampleOne {
+    public static void main(String[] args) {
+    System.out.println("Welcome to computer science");
+    System.out.println("Welcome to computer science");
+    System.out.println("Welcome to computer science");
+
+    System.out.println();
+
+    System.out.print("A");
+    System.out.print("B");
+    System.out.print("C");
+    System.out.println("D");
+    System.out.print("E");
+    System.out.println("F");
+
+}
+}
---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleTwo.java
@@ -0,0 +1,10 @@
+package day2;
+
+//This example is about escape squences
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("Computer Scie\nce is the \"BEST\" class ever");
+        System.out.println("/\\\"");
+    }
+}
