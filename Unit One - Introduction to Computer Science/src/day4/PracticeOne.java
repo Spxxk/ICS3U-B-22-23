4e223b855fbeae843ceea10aea864dc59890c33e
Owen Chen <ochen@bayviewglen.ca>
2022-09-21 11:07:28 -0400

    added classes

---
+++ Unit One - Introduction to Computer Science/src/day4/PracticeOne.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class PracticeOne {
+public static void main(String[] args) {
+    
+}
+    //finding the circumfrence of a circle
+
+    double radius = 4;
+    final double pi = 3.14;
+
+    double circumfernce = 2 * pi * radius;
+    System.out.println("")
+
+    
+}
