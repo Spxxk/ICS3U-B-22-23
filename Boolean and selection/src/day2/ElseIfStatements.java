9aa33e983f457ad5c0d3f84a51a949eef5f15313
Owen Chen <ochen@bayviewglen.ca>
2022-10-25 11:19:51 -0400

    Mog

---
+++ Boolean and selection/src/day2/ElseIfStatements.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class ElseIfStatements {
+    public static void main(String[] args) {
+        int grade = 67;
+        if (grade >= 95)
+            System.out.println("A+");
+        else if (grade >= 80)
+            System.out.println("A");
+        else if (grade >= 70)
+            System.out.println("B");
+        else if (grade >= 60)
+            System.out.println("C");
+        else if (grade >= 50)
+            System.out.println("D");
+        else 
+            System.out.println("F");
+
+        
+
+        
+
+    }
+    
+}
